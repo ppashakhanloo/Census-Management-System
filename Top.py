42e5e684f709f626917448f390f00b36cf95d859
Pardis <ppashakhanloo@gmail.com>
2016-02-21 15:15:11 +0330

    1st done.

--- Top.py
+++ Top.py
@@ -6,8 +6,18 @@ worksheet_male = workbook_male.sheet_by_name('ESTIMATES')
 worksheet_female = workbook_female.sheet_by_name('ESTIMATES')
 
 
-def get_data_by_country_year(country, year):
-    pass
+def get_data_by_country_year(worksheet_male, worksheet_female, country, year):
+    row_country_m, col_country_m = find_row_col_index(country, worksheet_male)
+    row_year_m, col_year_m = find_row_col_index(year, worksheet_male)
+
+    val_male = worksheet_male.cell(row_country_m, col_year_m).value
+
+    row_country_f, col_country_f = find_row_col_index(country, worksheet_female)
+    row_year_f, col_year_f = find_row_col_index(year, worksheet_female)
+
+    val_female = worksheet_female.cell(row_country_f, col_year_f).value
+
+    return val_male, val_female
 
 
 def find_row_col_index(string_value, sheet):
@@ -18,4 +28,5 @@ def find_row_col_index(string_value, sheet):
                     return i,j
     return None
 
-print(find_row_col_index("Burundi", workbook_male))
+
+print(get_data_by_country_year(worksheet_male, worksheet_female, "Djibouti", "1950"))

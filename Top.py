f0050b6613e6d338344085d698dd99f0a0e07923
Pardis Pashakhanloo <ppashakhanloo@gmail.com>
2016-02-24 14:50:43 +0330

    task 2 done.

--- Top.py
+++ Top.py
@@ -4,8 +4,8 @@ import os
 import Diagrammer
 
 
-workbook_male = xlrd.open_workbook('Data/WPP2015_POP_F01_2_TOTAL_POPULATION_MALE.XLS')
-workbook_female = xlrd.open_workbook('Data/WPP2015_POP_F01_3_TOTAL_POPULATION_FEMALE.XLS')
+workbook_male = xlrd.open_workbook('Data/WPP2015_POP_F01_2_TOTAL_POPULATION_MALE.XLS', formatting_info=True)
+workbook_female = xlrd.open_workbook('Data/WPP2015_POP_F01_3_TOTAL_POPULATION_FEMALE.XLS', formatting_info=True)
 worksheet_male = workbook_male.sheet_by_name('ESTIMATES')
 worksheet_female = workbook_female.sheet_by_name('ESTIMATES')
 workbook_pop_growth = xlrd.open_workbook('Data/WPP2015_POP_F02_POPULATION_GROWTH_RATE.XLS')
@@ -90,7 +90,6 @@ def get_data_by_country_year(worksheet_male, worksheet_female, country, year):
 
 
 def change_data_by_country_year(workbook_male, workbook_female, country, year, male_or_female, new_val):
-
     row_country_m, col_country_m = find_row_col_index(country, workbook_male.sheet_by_name('ESTIMATES'))
     row_year_m, col_year_m = find_row_col_index(year, workbook_male.sheet_by_name('ESTIMATES'))
 
@@ -102,14 +101,10 @@ def change_data_by_country_year(workbook_male, workbook_female, country, year, m
 
     if male_or_female == 'male':
         wb_male.get_sheet(0).write(row_country_m, col_year_m, new_val)
-        # os.remove('Data/WPP2015_POP_F01_2_TOTAL_POPULATION_MALE.XLS')
-        wb_male.save('male.XLS')
+        wb_male.save('Data/WPP2015_POP_F01_2_TOTAL_POPULATION_MALE.XLS')
     elif male_or_female == 'female':
         wb_female.get_sheet(0).write(row_country_f, col_year_f, new_val)
-        # os.remove('Data/WPP2015_POP_F01_2_TOTAL_POPULATION_FEMALE.XLS')
-        wb_female.save('female.XLS')
-
-    return 0
+        wb_female.save('Data/WPP2015_POP_F01_2_TOTAL_POPULATION_FEMALE.XLS')
 
 
 def find_row_col_index(string_value, sheet):
@@ -137,10 +132,10 @@ while True:
         print('Male: '+str(male_res)+'\n'+'Female: '+str(female_res)+'\n')
     else:
         if command == '2':
-            country = input('enter country: ')
-            year = input('enter year: ')
-            male_or_female = input('enter male or female: ')
-            new_val = input('enter new value: ')
+            country = raw_input('enter country: ')
+            year = raw_input('enter year: ')
+            male_or_female = raw_input('enter male or female: ')
+            new_val = raw_input('enter new value: ')
             change_data_by_country_year(workbook_male, workbook_female, country, year, male_or_female, new_val)
             print('Done.')
         else:
